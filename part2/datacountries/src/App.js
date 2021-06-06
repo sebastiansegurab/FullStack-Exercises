df85d6c5373b8de813cd115710cc9eff0e39aa2f
sebastiansegurab <sebsegura1905@gmail.com>
2021-06-05 23:29:33 -0500

    feat: Exercise 2.13

--- part2/datacountries/src/App.js
+++ part2/datacountries/src/App.js
@@ -4,6 +4,8 @@ import axios from 'axios'
 function App() {
   const [findName, setFindName] = useState('')
   const [countries, setCountries] = useState([])
+  const [showData, setShowData] = useState(false)
+  const [country, setCountry] = useState({})
 
   useEffect(() => {
     if (findName !== undefined && findName !== null && findName.trim() !== '') {
@@ -21,34 +23,44 @@ function App() {
 
   const returnCountries = () => {
     if (countries.length === 1) {
-      return (
-        <>
-          <h2>{countries[0].name}</h2>
-          <p>capital {countries[0].capital}</p>
-          <p>population {countries[0].population}</p>
-          <h3>languages</h3>
-          <ul>
-            {countries[0].languages.map((language) => {
-              return <li key={language.name}>{language.name}</li>
-            })}
-          </ul>
-          <img src={countries[0].flag} />
-        </>
-      )
+      return (returnDataCountry(countries[0]))
     }
     if (countries.length > 10) {
       return <p>Too many matches, specify another filter</p>
     } else {
       return countries.map((country) => {
-        return <p key={country.name}>{country.name}</p>
+        return <p key={country.name}>{country.name} <button onClick={() => handleClickShowData(country)}>show</button></p>
       })
     }
+  }
+
+  const returnDataCountry = (country) => {
+    return (
+      <>
+        <h2>{country.name}</h2>
+        <p>capital {country.capital}</p>
+        <p>population {country.population}</p>
+        <h3>languages</h3>
+        <ul>
+          {country.languages.map((language) => {
+            return <li key={language.name}>{language.name}</li>
+          })}
+        </ul>
+        <img src={country.flag} />
+      </>
+    )
+  }
+
+  const handleClickShowData = (country) => {
+    setShowData(!showData)
+    setCountry(country)
   }
 
   return (
     <>
       find countries <input onChange={handleFindName} value={findName} />
       {returnCountries()}
+      {showData ? returnDataCountry(country) : ''}  
     </>
   );
 }
