40075086f3321f99cb3a8dded7b49ab096d16626
sebastiansegurab <sebsegura1905@gmail.com>
2021-08-15 12:57:08 -0500

    feat: Exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,21 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+import '../App.css'
+
+const Blog = ({ blog }) => {
+  const [visibleBlog, setVisibleBlog] = useState(false)
+
+  const styleVisible = { display: visibleBlog ? '' : 'none' }
+
+  return (
+    <div className='blog-style'>
+      {blog.title} <button onClick={() => setVisibleBlog(!visibleBlog)}>{visibleBlog ? 'hide' : 'view'}</button><br />
+      <div style={styleVisible}>
+        {blog.url}<br />
+        likes {blog.likes}<br />
+        {blog.user.username}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
