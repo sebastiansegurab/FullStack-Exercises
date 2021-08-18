ca0d3ebb220cc0ddaef7ef89efbfcac7ce17ce8b
sebastiansegurab <sebsegura1905@gmail.com>
2021-08-18 00:38:08 -0500

    feat: Exercise 5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,29 +1,41 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import Blog from "./Blog";
 
-test("render a blog don't show its URL and likes by default", () => {
-  const blog = {
-    id: "12345",
-    title: "Title",
-    author: "Author",
-    url: "url",
-    likes: 1,
-    user: {
-        id: '12345',
-        name: 'Name',
-        username: 'Username'
-    }
-  };
+describe("render a blog", () => {
+  let component;
 
-  const component = render(
-    <Blog blog={blog} />
-  );
+  beforeEach(() => {
+    const blog = {
+      id: "12345",
+      title: "Title",
+      author: "Author",
+      url: "url",
+      likes: 1,
+      user: {
+        id: "12345",
+        name: "Name",
+        username: "Username",
+      },
+    };
+    component = render(<Blog blog={blog} />);
+  });
 
-  const divBlog = component.container.querySelector(".blog-style");
-  expect(divBlog).toHaveTextContent("Title");
-  expect(divBlog).toHaveTextContent("Author");
-  expect(divBlog).not.toHaveTextContent("url");
-  expect(divBlog).not.toHaveTextContent("likes");
+  test("doesn't show its URL and likes by default", () => {
+    const divBlog = component.container.querySelector(".blog-style");
+    expect(divBlog).toHaveTextContent("Title");
+    expect(divBlog).toHaveTextContent("Author");
+    expect(divBlog).not.toHaveTextContent("url");
+    expect(divBlog).not.toHaveTextContent("likes");
+  });
+
+  test("and click in view button shows its details", () => {
+    expect(component.container).not.toHaveTextContent("url");
+    expect(component.container).not.toHaveTextContent("likes");
+    const buttonToShowDetails = component.getByText("view");
+    fireEvent.click(buttonToShowDetails);
+    expect(component.container).toHaveTextContent("url");
+    expect(component.container).toHaveTextContent("likes");
+  });
 });
