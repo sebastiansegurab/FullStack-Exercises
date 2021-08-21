0bc7a09139a183514f2b35d3fe8577f94a0c94a4
sebastiansegurab <sebsegura1905@gmail.com>
2021-08-20 23:12:19 -0500

    feat: Exercise 5.15

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -5,6 +5,7 @@ import Blog from "./Blog";
 
 describe("render a blog", () => {
   let component;
+  const mockHandler = jest.fn();
 
   beforeEach(() => {
     const blog = {
@@ -19,7 +20,7 @@ describe("render a blog", () => {
         username: "Username",
       },
     };
-    component = render(<Blog blog={blog} />);
+    component = render(<Blog blog={blog} addLikeToBlog={mockHandler} />);
   });
 
   test("doesn't show its URL and likes by default", () => {
@@ -37,5 +38,14 @@ describe("render a blog", () => {
     fireEvent.click(buttonToShowDetails);
     expect(component.container).toHaveTextContent("url");
     expect(component.container).toHaveTextContent("likes");
+  });
+
+  test.only("and click in like button adds a new like for it", () => {
+    const buttonToShowDetails = component.getByText("view");
+    fireEvent.click(buttonToShowDetails);
+    const buttonLike = component.getByText("like");
+    fireEvent.click(buttonLike);
+    fireEvent.click(buttonLike);
+    expect(mockHandler.mock.calls).toHaveLength(2);
   });
 });
