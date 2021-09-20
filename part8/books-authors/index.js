a305321d1ed53afe29ee9294a32c8ecb0a2d3388
sebastiansegurab <sebsegura1905@gmail.com>
2021-09-19 22:38:31 -0500

    feat: Exercise 8.4

--- part8/books-authors/index.js
+++ part8/books-authors/index.js
@@ -106,7 +106,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String!): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -115,7 +115,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => books.filter(blog => blog.author === args.author),
     allAuthors: () => authors,
   },
   Author: {
