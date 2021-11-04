9c843c078c1b767b42303969d820ded9d2e12036
sebastiansegurab <sebsegura1905@gmail.com>
2021-11-03 23:09:22 -0500

    feat: Exercise 9.1

---
+++ part9/project1/bmiCalculator.ts
@@ -0,0 +1,29 @@
+const calculateBmi = (height: number, weight: number): string => {
+  if (height === 0 || weight === 0) {
+    throw new Error("Height or weight can't be 0");
+  }
+  const bmi: number = weight / (height / 100) ** 2;
+  if (bmi < 16) {
+    return "Underweight (Severe thinness)";
+  } else if (bmi >= 16 && bmi <= 16.9) {
+    return "Underweight (Moderate thinness)";
+  } else if (bmi >= 17 && bmi <= 18.4) {
+    return "Underweight (Mild thinness)";
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    return "Normal range";
+  } else if (bmi >= 25 && bmi <= 29.9) {
+    return "Overweight (Pre-obese)";
+  } else if (bmi >= 30 && bmi <= 34.9) {
+    return "Obese (Class I)";
+  } else if (bmi >= 35 && bmi <= 39.9) {
+    return "Obese (Class II)";
+  } else {
+    return "Obese (Class III)";
+  }
+};
+
+try {
+  console.log(calculateBmi(180, 74));
+} catch (error) {
+  console.log("Something went wrong, error message: ", error.message);
+}
