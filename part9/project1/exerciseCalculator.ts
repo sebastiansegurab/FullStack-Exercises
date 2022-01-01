e6f028516e3dd3a20c65b59e6c8b4d88ddc512b5
sebastiansegurab <51306076+sebastiansegurab@users.noreply.github.com>
2022-01-01 16:38:03 -0500

    feat: Exercise 9.2

---
+++ part9/project1/exerciseCalculator.ts
@@ -0,0 +1,39 @@
+interface Result {
+  periodLength: number,
+  trainingDays: number,
+  success: boolean,
+  rating: number,
+  ratingDescription: string,
+  target: number,
+  average: number
+}
+
+function calculateExercises(array: number[]): Result {
+  const trainingDays: number = array.filter(n => n > 0).length
+  let rating: number = 0
+  let ratingDescription: string = ""
+  const success: boolean = array.filter(n => n > 0).length === array.length
+  if (trainingDays > 5) {
+    rating = 3
+    ratingDescription = "Good work"
+  } else if (trainingDays <= 5 && trainingDays >= 3) {
+    rating = 2
+    ratingDescription = "You can try a little more"
+  } else {
+    rating = 1
+    ratingDescription = "Stop wasting time"
+  }
+  let result: Result = {
+    periodLength: array.length,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target: success ? 2 : 1,
+    average: array.reduce((a, b) => a + b, 0) / array.length
+  }
+  console.log(result)
+  return result
+}
+
+calculateExercises([3, 0, 2, 4.5, 0, 3, 1])
