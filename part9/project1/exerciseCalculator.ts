c1a969c8cc60f85f30f928695bee9ad4fbe59303
sebastiansegurab <51306076+sebastiansegurab@users.noreply.github.com>
2022-01-02 23:53:25 -0500

    feat: Exercise 9.6

--- part9/project1/exerciseCalculator.ts
+++ part9/project1/exerciseCalculator.ts
@@ -8,37 +8,22 @@ interface Result {
   average: number
 }
 
-const parseArguments
- = (args: Array<string>): Data => {
-  if (args.length < 9) throw new Error('Not enough arguments');
-  if (args.length > 9) throw new Error('Too many arguments');
-
-  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
-    return {
-      height: Number(args[2]),
-      weight: Number(args[3])
-    }
-  } else {
-    throw new Error('Provided values were not numbers!');
-  }
-}
-
 function calculateExercises(array: number[]): Result {
-  const trainingDays: number = array.filter(n => n > 0).length
-  let rating: number = 0
-  let ratingDescription: string = ""
-  const success: boolean = array.filter(n => n > 0).length === array.length
+  const trainingDays: number = array.filter(n => n > 0).length;
+  let rating = 0;
+  let ratingDescription = "";
+  const success: boolean = array.filter(n => n > 0).length === array.length;
   if (trainingDays > 5) {
-    rating = 3
-    ratingDescription = "Good work"
+    rating = 3;
+    ratingDescription = "Good work";
   } else if (trainingDays <= 5 && trainingDays >= 3) {
-    rating = 2
-    ratingDescription = "You can try a little more"
+    rating = 2;
+    ratingDescription = "You can try a little more";
   } else {
-    rating = 1
-    ratingDescription = "Stop wasting time"
+    rating = 1;
+    ratingDescription = "Stop wasting time";
   }
-  let result: Result = {
+  const result: Result = {
     periodLength: array.length,
     trainingDays,
     success,
@@ -46,9 +31,8 @@ function calculateExercises(array: number[]): Result {
     ratingDescription,
     target: success ? 2 : 1,
     average: array.reduce((a, b) => a + b, 0) / array.length
-  }
-  console.log(result)
-  return result
+  };
+  return result;
 }
 
-calculateExercises([3, 0, 2, 4.5, 0, 3, 1])
+calculateExercises([3, 0, 2, 4.5, 0, 3, 1]);
--- part9/project1/index.ts
+++ part9/project1/index.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express from 'express';
 import { calculateBmi } from './bmiCalculator';
 const app = express();
 
@@ -7,12 +7,12 @@ app.get('/', (_req, res) => {
 });
 
 app.get('/bmi', (req, res) => {
-    const { weight: weightP } = req.query
+    const { weight: weightP } = req.query;
     const { height: heightP } = req.query;
     if (!weightP || !heightP) {
-        res.status(404).json({error: "Bad request! Must pass all required parameters"});
+        res.status(404).json({ error: "Bad request! Must pass all required parameters" });
     } else if (isNaN(Number(weightP)) || isNaN(Number(heightP))) {
-        res.status(404).json({error: "Bad request. All parameters must be numbers"});
+        res.status(404).json({ error: "Bad request. All parameters must be numbers" });
     } else {
         res.status(200).json(
             {
